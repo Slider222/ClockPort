2f4fd2f8f9312b0396c1bae5625d95fdbd73ca58
Sergey <sorokinap2011@yandex.ru>
2022-02-25 15:26:56 +0300

    new repo

---
+++ src/javaclockport/JavaClockPort.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaclockport;
+
+import javax.swing.JFrame;
+
+public class JavaClockPort {
+    
+    
+    public static void main(String[] args) {
+       MainFrame MyFrame = new MainFrame();
+       MyFrame.main(null);
+       
+    }
+    
+}
